2a4e9b8d98d8bee4b652f9e6d7829a3eadce4bdc
wlb <wlb1004@163.com>
2022-04-18 10:58:07 +0800

    second commit

--- SpringMVC_init/src/main/java/com/company/mvc/controller/TestController.java
+++ SpringMVC_init/src/main/java/com/company/mvc/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @RequestMapping(value = "/")
     public String index(){
+        System.out.println("");
         return "index";
     }
 }
