98944e8e72b633f96e36261a48f78b72cb622006
wlb <wlb1004@163.com>
2022-04-18 16:16:37 +0800

    try commit

--- SpringMVC_demo4/src/main/java/com/company/mvc/controller/HttpController.java
+++ SpringMVC_demo4/src/main/java/com/company/mvc/controller/HttpController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
